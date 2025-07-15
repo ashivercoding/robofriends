43fb9a0b34a6b0f02266cd52559b319fa1f67132
Amanda Shiver <a.shiver192@gmail.com>
2025-07-15 14:21:16 -0400

    pwa changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Robofriends',
+        short_name: 'VitePWA',
+        description: 'A searchable database of robots',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        screenshots: [
+          {
+            src: '',
+            sizes: '1280x512',
+            type: 'image/png',
+            form_factor: 'wide',
+          },
+        ],
+      },
+    }),
+  ],
   base: '/robofriends/'
 })
